9c5378e9f0e022177017194d7804fef2d847a11f
jacmar <jacopo.marello@gmail.com>
2025-04-04 12:25:20 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'selective-focus-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// Installazione del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Attivazione: pulisci cache vecchie
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Strategia di caching: Network First, fallback su Cache
+self.addEventListener('fetch', event => {
+  // Ignora le richieste che non sono GET
+  if (event.request.method !== 'GET') return;
+  
+  // Ignora le ri
